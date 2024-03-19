4302438b5727e09203356d6b53ca402495f086e9
moussimene <mlle.moussaoui.imene@gmail.com>
2024-03-19 11:40:57 +0100

    version finale fizzbuz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,28 @@
 let i=0;
 for (i=1; i<=100; i++){
-    console.log(i)
-}
+    
+    if (i%3===0 && i%5===0){
+        console.log("fizz buzz")
+    }
+    else{
+             
+    if (i%3===0){
+        console.log("fizz");
+    }
+    else{
+        if(i%5===0){
+            console.log("buzz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+   
+  
+    
+    
+  
+    }
 
+
+}
